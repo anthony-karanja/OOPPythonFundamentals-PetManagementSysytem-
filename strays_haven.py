38b78098dd89833ca6e9fdb676d108a6605273fe
Anthony <wangonguanthony@gmail.com>
2025-05-14 10:36:47 +0300

    class dog initialized

---
+++ strays_haven.py
@@ -0,0 +1,31 @@
+class Pet:
+    def speak(self):
+        print("Sound made")
+        return ("pet spoke")
+    
+Rasmus = Pet()
+Rasmus.name = Rasmus
+print(Rasmus.name)
+print(Rasmus.speak())
+
+class Dog:
+    def __init__(self,name,breed,age="N/A"):
+        self.name = name
+        self.breed = breed
+        self.age = age
+
+    def speak(self):
+        return f"{self.name} says woof!"
+    
+koba = Dog("koba, Great Dane", 3)
+amad = Dog("Amad, Black goat")
+koba.age = 4
+print(koba.speak())
+print(koba.age)
+print(amad.age)
+
+class Cat:
+    pass
+
+class Rat:
+    pass
